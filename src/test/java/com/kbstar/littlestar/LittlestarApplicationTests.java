db1e7529a45f680f240a1caada07a3eac3e5f766
SangHoon <112858332+Hoonssac@users.noreply.github.com>
2025-05-01 12:53:34 +0900

    First commit

---
+++ src/test/java/com/kbstar/littlestar/LittlestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar.littlestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittlestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
