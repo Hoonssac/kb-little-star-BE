3d948a49bc5f7fc26b06ae0a034e28ddfe84068d
hoonssac <Hoonssac.dev@gmail.com>
2025-07-02 14:22:50 +0900

    refactor: Return updated user info on success for gacha and main Pokemon update APIs

--- src/main/java/com/kbstar/littlestar/user/controller/UserPokemonController.java
+++ src/main/java/com/kbstar/littlestar/user/controller/UserPokemonController.java
@@ -1,9 +1,12 @@
 package com.kbstar.littlestar.user.controller;
 
+import com.kbstar.littlestar.user.domain.User;
 import com.kbstar.littlestar.user.dto.GatchaPokemonRequest;
 import com.kbstar.littlestar.user.dto.UpdateMainPokemonRequest;
-import com.kbstar.littlestar.user.service.UserPokemonService;
+import com.kbstar.littlestar.user.dto.UserResponse;
 
+import com.kbstar.littlestar.user.service.UserPokemonService;
+import com.kbstar.littlestar.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class UserPokemonController {
 
     private final UserPokemonService userPokemonService;
+    private final UserService userService;
 
     @PatchMapping("/main-pokemon")
     public ResponseEntity<?> updateMainPokemon(@RequestBody UpdateMainPokemonRequest request, HttpSession session) {
         userPokemonService.updateMainPokemon(request.getMainPokemonId(), session);
-        return ResponseEntity.ok("대표 포켓몬이 업데이트되었습니다.");
+        User user = (User) session.getAttribute("user");
+        UserResponse response = userService.toUserResponse(user);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/gatcha")
     public ResponseEntity<?> gatchaPokemon(@RequestBody GatchaPokemonRequest request, HttpSession session) {
         userPokemonService.gatchaPokemon(request.getNewPokemonId(), session);
-        return ResponseEntity.ok("새로운 포켓몬이 추가되었습니다.");
+        User user = (User) session.getAttribute("user");
+        UserResponse response = userService.toUserResponse(user);
+        return ResponseEntity.ok(response);
     }
 }
